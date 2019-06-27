948782dc2e31d1f7c9266182d912345adaed2b7c
Navid Hariri <navidhariri25@gmail.com>
2019-06-26 19:27:23 -0700

    fixed STATIC_ROOT I THINK!??!

--- wall/settings.py
+++ wall/settings.py
@@ -139,8 +139,11 @@ USE_TZ = True
 # STATIC_DIR = os.path.join(BASE_DIR, "Templates/static")
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+static_file = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    STATIC_ROOT
+    static_file
 ]
