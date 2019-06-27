29de468e5552e34f3ba050f6c55c62f9b9352c83
Navid Hariri <navidhariri25@gmail.com>
2019-06-26 19:38:04 -0700

    static_root from pythonanywhere

--- wall/settings.py
+++ wall/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+os.path.join(BASE_DIR, "static")
 
 static_file = os.path.join(BASE_DIR, "static")
 
