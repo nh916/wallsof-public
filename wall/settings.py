8d8af4941ba1b906077c9197257fd7fd39087248
Navid Hariri <navidhariri25@gmail.com>
2019-06-27 16:55:57 -0700

    allowed hosts

--- wall/settings.py
+++ wall/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2%aqt6vb@-9=75+mi9x901+vs-qas1%kd6f_z_v=vpz)ta-d+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#######################@@@@@@@@@@!!!!!!!!!............
 DEBUG = True
 
 ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['AugustusCaesar.pythonanywhere.com', 'www.thewallsof.com']
+# ALLOWED_HOSTS = ['thewallsof.com']
+# ALLOWED_HOSTS = ['thewallsof.com', 'www.thewallsof.com', 'AugustusCaesar.pythonanywhere.com']
 
 
 # Application definition
